eb8f29f1a73c4d600264e0374045e60dacc26afc
Vladimir Ladygin <vladygin@caltech.edu>
2025-07-30 12:10:57 -0700

    Added new example

---
+++ examples/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+boltz predict affinity_prot_prot.yaml --model boltz2_ensemble --use_msa_server --cache=/pscratch/sd/v/vladygin/side_projects/ML_coding_series/Boltz-tests/.boltz --override
